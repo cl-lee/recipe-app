31737ed1f5ef7670a4676293b3c47ffe0b778f0f
pandi1813 <andrea.peter1813@gmail.com>
2023-03-16 20:26:05 +0000

    created pseudocode in readme file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import './App.css';
+fetch("https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=901555f1&app_key=42eb9c88a0deb3aae3cef1a802a1283b")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
 
 function App() {
   return (
