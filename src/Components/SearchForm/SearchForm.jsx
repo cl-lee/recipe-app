22a7676a03d49ca7bf64cacfc63fecc845d9a0a2
pandi1813 <andrea.peter1813@gmail.com>
2023-03-17 18:47:39 +0000

    linked ingredientList state to input field and console logged the input on clicking add button

--- src/Components/SearchForm/SearchForm.jsx
+++ src/Components/SearchForm/SearchForm.jsx
@@ -1,11 +1,33 @@
+import { useState } from "react"
 
 
 export default function SearchForm() {
+
+const [ingredientList, setIngredientList] = useState('');
+
     return (
         <>
-        <form action="">
-            <input type="text" name="ingredient" id="ingredient"  placeholder="eg: tomato"/>
+        <form >
+
+            <h2>List your ingredients</h2>
+            <input 
+            type="text"
+            name="ingredient"
+            id="ingredient" 
+            required
+            value={ingredientList}
+            onChange={(e) =>setIngredientList(e.target.value) }/>
+            <button onClick={(e) =>{
+                e.preventDefault();
+                // ingredientArray.push(ingredientList)
+                console.log(ingredientList)
+            }} id="add">Add</button>
+            <ul>
+
+            </ul>
+            
         </form>
+       
         </>
     )
 }
