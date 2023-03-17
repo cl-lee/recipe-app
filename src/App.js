d3b8ff044b1a85bda01a20f2b2c8f6ab77de37c5
EdgarsCer <Edgarscernijs1993@gmail.com>
2023-03-17 19:46:25 +0000

    Home page added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
-import "./App.css";
-import "../src/Components/Home";
-import "../src/Components/MainPicture";
-import "../src/Components/Option1";
-import "../src/Components/Option2";
-import "../src/Components/Option3";
+import React from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import Home from './Components/Home';
 
 function App() {
   return (
-    <article class="message">
-      <div class="message-header">
-        <p>test bulma</p>
-        <button class="delete" aria-label="delete"></button>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/page1">Page 1</Link>
+            </li>
+            <li>
+              <Link to="/page2">Page 2</Link>
+            </li>
+            <li>
+              <Link to="/page3">Page 3</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/page1">
+            <h1>Page 1</h1>
+          </Route>
+          <Route path="/page2">
+            <h1>Page 2</h1>
+          </Route>
+          <Route path="/page3">
+            <h1>Page 3</h1>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
-      <div class="message-body">test bulma</div>
-    </article>
+    </BrowserRouter>
   );
 }
 
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,15 +1,15 @@
-
+import React from 'react';
 
 function Home() {
-    return (
-      <p>Something</p>
-    );
-  }
-
-
-
-
-
-
+  return (
+    <div>
+      <h1>Welcome to My Website!</h1>
+      <p>This is some text about my website.</p>
+      <button>Option 1</button>
+      <button>Option 2</button>
+      <button>Option 3</button>
+    </div>
+  );
+}
 
 export default Home;
