7ceeb77474bbebbf5ca1212f68d89a56e204d410
cl.lee <cl.lee.ofz@gmail.com>
2023-03-18 15:13:48 +0000

    added Recipe css file, updated Recipe component styling

--- src/Components/Recipe.js
+++ src/Components/Recipe.js
@@ -1,42 +1,37 @@
 import ResultCarousel from "./ResultCarousel";
 import sampleData from "./temp.json";
+import "./Recipe.css";
+let searchResults = sampleData.hits;
 
 function Recipe() {
   return (
     <div>
       {/* NAVIGATION BAR */}
-      <nav className="navbar is-black">
-        <div className="navbar-brand">
-          {/* Logo */}
-          <a className="navbar-item" href="https://bulma.io">
-            <img
-              src="https://bulma.io/images/bulma-logo-white.png"
-              alt="Bulma: a modern CSS framework based on Flexbox"
-              width="112"
-              height="28"
-            />
-          </a>
-          {/* Home button */}
-          <div class="navbar-item">
-            <div class="buttons">
-              <a class="button is-primary">
-                <strong>Sign up</strong>
-              </a>
-            </div>
-          </div>
-        </div>
+      <nav id="nav-bar">
+        {/* Logo */}
+        <img
+          id="logo"
+          src="https://bulma.io/images/placeholders/128x128.png"
+          alt="Logo"
+        />
+        {/* Home button */}
+        <button id="home-button">Home</button>
       </nav>
-
-      {/* PAGE TITLE */}
-      <h1 class="title">What you can make</h1>
-
-      {/* RECIPE CARD (ResultCarousel) */}
-      <ResultCarousel></ResultCarousel>
+      <main>
+        <header id="recipe-page-header">
+          {/* PAGE TITLE */}
+          <h1 id="recipe-page-title">What you can make</h1>
+        </header>
+        {/* RECIPE CARD (ResultCarousel) */}
+        <ResultCarousel></ResultCarousel>
+      </main>
 
       {/* EDIT SEARCH BUTTON */}
       {/* goes back to last page with saved results */}
       {/* make button bigger */}
-      <button class="button is-responsive">Default</button>
+      <footer>
+        <button className="button is-responsive">Edit Search</button>
+      </footer>
     </div>
   );
 }
@@ -45,7 +40,7 @@ export default Recipe;
 
 // --- pseudocode ---
 // CSS:
-// background-color: #F4DF20; 
+// background-color: #F4DF20;
 // add a couple of food/dishes images (see wireframe);
 // card background-color: #00AF54, opacity: 0.6;
 // button background-color: #B6671E;
@@ -59,12 +54,12 @@ export default Recipe;
 
 // show four results per carousel page
 // logic: start with array index 0, 1, 2, 3
-// if clicked right button: 
+// if clicked right button:
 // next four items in array (index 4, 5, 6, 7)
 
 // for loop:
 // for (var i = 0; i < 4; i++) {
-  // console.log(numbers[i]);
+// console.log(numbers[i]);
 // }
 
 // map and spread?
--- src/Components/ResultCarousel.js
+++ src/Components/ResultCarousel.js
@@ -1,27 +1,27 @@
 function ResultCarousel(props) {
-  return (
-    <div class="card">
-      <div class="card-content">
-        <div class="media">
-          <div class="media-left">
-            <figure class="image is-96x96">
-              <img
-                src="https://bulma.io/images/placeholders/96x96.png"
-                alt="Placeholder image"
-              />
-            </figure>
-          </div>
-          <div class="media-content">
-            <p class="title is-4">John Smith</p>
-            <div class="content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-              nec iaculis mauris.
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  // return (
+  //   <div class="card">
+  //     <div class="card-content">
+  //       <div class="media">
+  //         <div class="media-left">
+  //           <figure class="image is-96x96">
+  //             <img
+  //               src={props.recipe.image}
+  //               alt={props.recipe.label}
+  //             />
+  //           </figure>
+  //         </div>
+  //         <div class="media-content">
+  //           <p class="title is-4">{props.recipe.label}</p>
+  //           <div class="content">
+  //             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+  //             nec iaculis mauris.
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 }
 
 export default ResultCarousel;
@@ -30,3 +30,22 @@ export default ResultCarousel;
 // LOGIC:
 // use props
 // card is a clickable link to the actual recipe page (nest card div in <a> tag?)
+// to render:
+// image: 
+// props.recipe.images['THUMBNAIL'].url
+
+// dishname:
+// props.recipe.label
+
+// time needed:
+// props.recipe.totalTime
+
+// number of ingredients:
+// props.recipe.ingredients.length
+
+// servings:
+// props.recipe.ingredients.yield
+
+// meal type:
+// "Best for: " 
+// props.recipe.ingredients.mealType
