5172bbeb02ac00ba988d79ee27a537b87b1bda45
pandi1813 <andrea.peter1813@gmail.com>
2023-03-19 17:03:08 +0000

    added fruit basket image to public folder

--- src/Components/SearchPage.js
+++ src/Components/SearchPage.js
@@ -1,10 +1,10 @@
 import SearchForm from "./SearchForm/SearchForm";
 
-
 export default function SearchPage() {
     return (
       <>
       <SearchForm/>
+      <img src='./images/veggieBasket.png' alt="healthy ingredients"/>
       </>
     );
   }
