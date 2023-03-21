1597fe3087f0aa6231057a0873b16a1e929f1d59
pandi1813 <andrea.peter1813@gmail.com>
2023-03-21 18:53:30 +0000

    fixed main page loading

--- src/Components/SearchPage.js
+++ src/Components/SearchPage.js
@@ -1,8 +1,10 @@
 import SearchForm from "./SearchForm/SearchForm";
+import Navbar from "./Navbar";
 
 export default function SearchPage() {
     return (
       <>
+      <Navbar/>
       <SearchForm/>
       
       </>
