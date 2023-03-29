34dae567c3cd2462af84418dca302cc3bb230eaa
cl.lee <cl.lee.ofz@gmail.com>
2023-03-29 14:31:48 +0100

    updated Home.js comments, removed redundant codes from Home.css

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import "./Home.css";
 import image1 from "../images/image1.jpg";
 import image2 from "../images/image2.jpg";
 import image3 from "../images/image3.jpg";
-import { NavLink } from "react-router-dom";
-import "./Home.css";
 
-/* div: className="container is-fluid" */
-/* id="home-page": className="is-centered " */
-function Home() {
+export default function Home() {
   const [activeIndex, setActiveIndex] = useState(0);
   const images = [image1, image2, image3];
 
@@ -21,14 +19,17 @@ function Home() {
 
   return (
     <section class="hero is-fullheight m-0 p-0">
-      {/* <!-- Hero head: essential component for fullheight hero --> */}
+      {/* <!-- Hero head: essential component for Bulma fullheight hero --> */}
       <div id="carousel-hero-head m-0 p-0" class="hero-head"></div>
 
-      {/* <!-- Hero content: will be in the middle --> */}
+      {/* <!-- Bulma hero-body --> */}
       <div class="hero-body m-0 p-0">
-        <div id="home-page-columns" class="columns is-tablet is-flex-row-reverse m-0 p-0">
-          <div id="carousel" class="column is-tablet-half m-0 p-0">
-            <div className="carousel">
+        <div
+          id="home-page-columns"
+          class="columns is-tablet is-flex-row-reverse m-0 p-0"
+        >
+          <div id="carousel-column" class="column is-tablet-half m-0 p-0">
+            {/* <div id="carousel-container"> */}
               {images.map((image, index) => (
                 <img
                   key={index}
@@ -39,25 +40,21 @@ function Home() {
                   }`}
                 />
               ))}
-            </div>
+            {/* </div> */}
           </div>
-          <div id="home-page-panel" class="column is-tablet-half m-0 p-0">
+          <div id="logo-column" class="column is-tablet-half m-0 p-0">
             <img id="logo" src="./logo-1.png" alt="Meal-Mate logo" />
             <div id="start-button-container" class="buttons is-centered">
               <NavLink to="/search">
-                <button className="button is-primary">
-                  Get Started
-                </button>
+                <button className="button is-primary">Get Started</button>
               </NavLink>
             </div>
           </div>
         </div>
       </div>
 
-      {/* <!-- Hero footer: essential component for fullheight hero --> */}
+      {/* <!-- Hero footer: essential component for Bulma fullheight hero --> */}
       <div class="hero-foot"></div>
     </section>
   );
 }
-
-export default Home;
